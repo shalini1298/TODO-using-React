24aabded7a2a2fefb44ca2dafc9a1ac135295fe3
shalini <48598782+shalini1298@users.noreply.github.com>
2019-11-26 15:24:39 +0530

    Add files via upload

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Title = () => {
+    return (
+      <div>
+         <div>
+            <h1>TO-DO</h1>
+         </div>
+      </div>
+    );
+  }
+  export default Title
